bbc1d84c6bb9df50a4c4dfe4a2d72d1ce905eaf2
nauanelinhares <nauane.linhares@gmail.com>
2025-03-23 17:28:27 -0300

    Refactored prompt message in main function to remove "testando" for a cleaner user experience.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
-    print!("$ testando");
+    print!("$ ");
     io::stdout().flush().unwrap();
 
     // Wait for user input
