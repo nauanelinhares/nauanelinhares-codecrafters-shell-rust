d71c9e843b878748df5f0400b010f81a9a61e090
nauanelinhares <nauane.linhares@gmail.com>
2025-03-30 22:35:06 -0300

    feat: add ~ on cd

--- src/built_in_commands.rs
+++ src/built_in_commands.rs
@@ -18,10 +18,16 @@ pub fn pwd_command() {
 
 pub fn cd_command(path: &str) {
     let current_dir = env::current_dir().unwrap();
-    let new_path = current_dir.join(path);
-    let result = env::set_current_dir(new_path);
 
-    if result.err().is_some() {
-        println!("cd: {}: No such file or directory", path)
+    if path == "~" {
+        let home = env::var("HOME").unwrap();
+        let _ = env::set_current_dir(home);
+    } else {
+        let new_path = current_dir.join(path);
+        let result = env::set_current_dir(new_path);
+
+        if result.err().is_some() {
+            println!("cd: {}: No such file or directory", path)
+        }
     }
 }
